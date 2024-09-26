12e5563709097f693a40b95ae4684d5cdd174428
rossella pellizzi <prellizzi@gmail.com>
2024-09-26 18:06:21 +0200

    rspec for product model

---
+++ spec/product_spec.rb
@@ -0,0 +1,23 @@
+require_relative "../app/product.rb"
+
+RSpec.describe Product do
+  it 'creates a product class' do
+    product = Product.new
+    expect(product).to be_kind_of(Product)
+  end
+
+  it 'should have a name' do
+    product = Product.new
+    expect(product).to respond_to :name
+  end
+
+  it 'should have a product_code' do
+    product = Product.new
+    expect(product).to respond_to :product_code
+  end
+
+  it 'should have a price' do
+    product = Product.new
+    expect(product).to respond_to :price
+  end
+end
