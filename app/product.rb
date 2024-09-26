7ca6fbb300ca50ecfab3eb7850ef5c6193a4e533
rossella pellizzi <prellizzi@gmail.com>
2024-09-26 18:09:51 +0200

    product model set

--- app/product.rb
+++ app/product.rb
@@ -1,3 +1,12 @@
 class Product
-  
+  attr_reader :product_code, :name, :price
+  attr_accessor :discount, :id
+
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @product_code = attributes[:product_code]
+    @name = attributes[:name]
+    @price = attributes[:price]
+    @discount = attributes[:discount] || nil
+  end
 end
