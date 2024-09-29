50731eef0bb71d2bd5a9986cd9819c759d46324b
rossella pellizzi <prellizzi@gmail.com>
2024-09-29 08:46:48 +0200

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ProductList from './components/ProductList';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar />
     <div className="container">
       <h1>Welcome to Our Shop</h1>
       <ProductList />
     </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <i class="fa-solid fa-barcode"></i>
+        <p>Just a Shop</p>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
