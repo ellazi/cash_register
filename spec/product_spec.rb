03dfd0e398f2e76aba7a410763e4eed6083fd8ea
rossella pellizzi <prellizzi@gmail.com>
2024-09-29 11:30:13 +0200

    rspec cleaned

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,24 +1,22 @@
 require_relative '../app/models/product'
 
 RSpec.describe Product do
+  let(:product) { Product.new }
+
   it 'creates a product class' do
-    product = Product.new
     expect(product).to be_kind_of(Product)
   end
 
   describe '#initialize' do
     it 'should have a name' do
-      product = Product.new
       expect(product).to respond_to :name
     end
 
     it 'should have a product_code' do
-      product = Product.new
       expect(product).to respond_to :product_code
     end
 
     it 'should have a price' do
-      product = Product.new
       expect(product).to respond_to :price
     end
   end
--- spec/products_controller_spec.rb
+++ spec/products_controller_spec.rb
@@ -2,79 +2,61 @@ require_relative '../app/controllers/products_controller'
 require_relative '../app/repositories/product_repository'
 require_relative '../app/views/products_view'
 
-RSpec.describe "ProductsController", :product do
-  let(:products) do
-    [
-      [ "id", "product_code", "name", "price", "discount" ],
-      [ 1, "GR1", "Green Tea", 3.11, "Buy one get one free" ],
-      [ 2, "SR1", "Strawberries", 5.0, "Fifty cents off" ],
-      [ 3, "CF1", "Coffee", 11.23, "One third off" ]
-    ]
-  end
+RSpec.describe "ProductsController" do
   let(:csv_path) { "spec/support/products.csv" }
   let(:repository) { ProductRepository.new(csv_path) }
+  let(:view) { ProductsView.new }
+  let(:controller) { ProductsController.new(repository) }
+
+  before do
+    allow(STDOUT).to receive(:puts)  # Suppress output from puts
+  end
 
   describe "#initialize" do
     it "should be initialized with a ProductRepository`" do
-      controller = ProductsController.new(repository)
       expect(controller).to be_a(ProductsController)
     end
 
     it "inizializes a cart array" do
-      expect(ProductsController.new(repository).cart).to be_a(Array)
+      expect(controller.cart).to be_a(Array)
     end
 
     it "inizializes a view instance" do
-      view = ProductsView.new
       expect(view).to be_a(ProductsView)
     end
   end
 
   describe "#add" do
     it "should ask the user for a product id, then add to cart" do
-      controller = ProductsController.new(repository)
-
       allow_any_instance_of(Object).to receive(:gets).and_return("2", "1")
       controller.add
 
       expect(controller.cart.length).to eq(1)
     end
-  end
 
-  describe "#list" do
-    it "should display all products in the repository" do
-      controller = ProductsController.new(repository)
+    it "should display an error message if the product does not exist" do
+      allow(repository).to receive(:find).with(4).and_return(nil)
 
-      expect(STDOUT).to receive(:puts).with("| Id | Product Code | Name | Price |")
-      expect(STDOUT).to receive(:puts).with("| -- | ------------ | ---- | ----- |")
-      products.drop(1).each do |product|
-        expect(STDOUT).to receive(:puts).with("| #{product[0]} | #{product[1]} | #{product[2]} | #{product[3]} € |")
-      end
+      expect(STDOUT).to receive(:puts).with("Type a valid id.")
 
-      controller.list
+      result = repository.find(4)
+      puts "Type a valid id." if result.nil?
     end
   end
 
   describe "#checkout" do
-    it "should display the cart and the total price" do
-      controller = ProductsController.new(repository)
-
-      allow_any_instance_of(Object).to receive(:gets).and_return("2", "1")
+    it "should have a basket with products code and total price" do
+      allow_any_instance_of(Object).to receive(:gets).and_return("2", "2")
       controller.add
 
-      expect(STDOUT).to receive(:puts).with("Your cart:")
-      expect(STDOUT).to receive(:puts).with("| Basket | Total price expected |")
-      expect(STDOUT).to receive(:puts).with("| ------ | -------------------- |")
-      expect(STDOUT).to receive(:puts).with("| #{controller.cart[0].product_code} |  #{controller.cart[0].price} € |")
-
-      controller.checkout
+      expect(controller.cart.length).to eq(2)
+      expect(controller.cart.map(&:product_code)).to eq(["SR1", "SR1"])
+      expect(controller.total).to eq(10.00)
     end
   end
 
   describe "#buy_one_get_one_free" do
     it "should calculate the total price for the 'buy_one_get_one_free' discount" do
-      controller = ProductsController.new(repository)
-
       allow_any_instance_of(Object).to receive(:gets).and_return("1", "2")
       controller.add
 
@@ -84,8 +66,6 @@ RSpec.describe "ProductsController", :product do
 
   describe "#fifty_cents_off" do
     it "should calculate the total price for the 'fifty_cents_off' discount" do
-      controller = ProductsController.new(repository)
-
       allow_any_instance_of(Object).to receive(:gets).and_return("2", "3")
       controller.add
 
@@ -95,8 +75,6 @@ RSpec.describe "ProductsController", :product do
 
   describe "#one_third_off" do
     it "should calculate the total price for the 'one_third_off' discount" do
-      controller = ProductsController.new(repository)
-
       allow_any_instance_of(Object).to receive(:gets).and_return("3", "3")
       controller.add
 
@@ -106,8 +84,6 @@ RSpec.describe "ProductsController", :product do
 
   describe "#total" do
     it "should calculate the total price" do
-      controller = ProductsController.new(repository)
-
       allow_any_instance_of(Object).to receive(:gets).and_return("1", "2")
       controller.add
 
